bea1221213fdafb76157787d1b886fb5e176751b
fkaczmarczyk <filip.kaczmarczyk@aibron.pl>
2024-10-14 10:37:20 +0200

    created utils function to showing image on plot

--- quality-module/utils.py
+++ quality-module/utils.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import imageio
 import numpy as np
-import cv2
 from matplotlib import pyplot as plt
 
 
@@ -12,7 +11,19 @@ def read_image(_img_path):
     :param _img_path: Path of input fingerprint image
     :return: numpy array of floats
     """
+
     return imageio.v3.imread(_img_path).astype(np.float64)
+
+
+def show_image_on_plot(_img, _title=None):
+    """
+    Function to show the image in grayscale on plot
+    :param _img: Read image
+    :param _title: Title of subplot of plot
+    """
+
+    plt.figure().suptitle(_title)
+    plt.imshow(_img, cmap='gray')
 
 
 folder = Path(r'C:\Users\Filip\Desktop\STUDIA\inzynierka\CrossMatch_Sample_DB')
@@ -20,7 +31,8 @@ tif_files = list(folder.glob('*.tif'))
 
 for tif_file in tif_files:
     img = read_image(tif_file)
+    show_image_on_plot(img, tif_file)
+    plt.show()
     # plt.figure().suptitle('ez')
     # plt.imshow(img, cmap="gray")
     # plt.show()
-
