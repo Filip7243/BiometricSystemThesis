14f642760d1f597a64ae1076dcc7216e84f855d8
fkaczmarczyk <filip.kaczmarczyk@aibron.pl>
2024-10-14 11:02:54 +0200

    added normalization function - max method

--- quality-module/utils.py
+++ quality-module/utils.py
@@ -26,13 +26,30 @@ def show_image_on_plot(_img, _title=None):
     plt.imshow(_img, cmap='gray')
 
 
-folder = Path(r'C:\Users\Filip\Desktop\STUDIA\inzynierka\CrossMatch_Sample_DB')
-tif_files = list(folder.glob('*.tif'))
+def normalize_image(_img):
+    """
+    Function that normalizes image to values in range [0, 1]
+    with max normalization from: https://research.ijcaonline.org/volume32/number10/pxc3875530.pdf section 4.5
+    :param _img: Input fingerprint image
+    :return: Normalized image between [0, 1]
+    """
+    _img = np.copy(_img)
+
+    max_val = np.max(_img)
+    if max_val > 0.0:
+        _img /= max_val
+
+    return _img
+
+# folder = Path(r'C:\Users\Filip\Desktop\STUDIA\inzynierka\CrossMatch_Sample_DB')
+folder = Path(r'C:\Users\Filip\Desktop\STUDIA\inzynierka\CrossMatch_Sample_DB\images\500\png\plain')
+tif_files = list(folder.glob('*.png'))
 
 for tif_file in tif_files:
     img = read_image(tif_file)
     show_image_on_plot(img, tif_file)
+
+    norm_img = normalize_image(img)
+    show_image_on_plot(norm_img, tif_file)
+    print(np.min(norm_img), np.max(norm_img))
     plt.show()
-    # plt.figure().suptitle('ez')
-    # plt.imshow(img, cmap="gray")
-    # plt.show()
